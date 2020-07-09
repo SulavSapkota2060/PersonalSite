364a15215b51922e2b038fccf9f3aca446f7984a
Sulav Sapkota <sulavsapkota100@gmail.com>
2020-07-09 12:54:26 +0545

    Changes

--- BloggingSite/settings.py
+++ BloggingSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4al#k9-5^+^*hlspcstr)s$ct821ghh@kzikw+3yi*4@xkh4j^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sulavsapkota.herokuapp.com','127.0.0.1']
 
